171d975f5945817fce26f9edb190ade6b71efca2
AngKokLeong <ang.kok.leong.development@gmail.com>
2024-01-17 11:12:18 +0000

    added commands to remove gunzip archive files

--- ubuntu_os_nagios_setup/setup.bash
+++ ubuntu_os_nagios_setup/setup.bash
@@ -10,6 +10,7 @@ apt-get install -yq autoconf gcc libc6 make wget unzip apache2 php libapache2-mo
 wget --inet4-only --content-disposition 'https://go.nagios.org/get-core/4-5-0/' -O nagios-4.5.0.tar.gz
 
 tar -xzvf nagios-4.5.0.tar.gz
+rm nagios-4.5.0.tar.gz
 
 cd nagios-4.5.0
 
@@ -26,5 +27,5 @@ cd /home/dockeradm/Desktop
 wget --inet4-only --content-disposition 'https://github.com/nagios-plugins/nagios-plugins/archive/refs/tags/2.4.8.tar.gz' -O nagios-plugins-2.4.8.tar.gz
 
 tar -xzvf nagios-plugins-2.4.8.tar.gz
-
+rm nagios-plugins-2.4.8.tar.gz
 
