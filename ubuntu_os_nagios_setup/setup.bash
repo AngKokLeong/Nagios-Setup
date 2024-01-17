e0f471aed03aab6a76457e03fb43955fff54bdf1
AngKokLeong <ang.kok.leong.development@gmail.com>
2024-01-17 15:18:49 +0000

    added commands to install docker

--- ubuntu_os_nagios_setup/setup.bash
+++ ubuntu_os_nagios_setup/setup.bash
@@ -9,6 +9,27 @@ apt-get update -yq
 
 apt-get install -yq autoconf gcc libc6 make wget unzip apache2 php libapache2-mod-php7.4 libgd-dev
 
+
+
+#Setup Docker
+
+# Add Docker's official GPG key:
+apt-get install ca-certificates curl gnupg -yq
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+   tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -yq
+
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -yq
+
+
+
 #Retrieve from this link 'https://www.nagios.org/downloads/nagios-core/thanks/'
 wget --inet4-only --content-disposition 'https://go.nagios.org/get-core/4-5-0/' -O nagios-4.5.0.tar.gz
 
