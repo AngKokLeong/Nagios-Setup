81ddc6b312ae1b2169e5a20930d477861248bdb2
AngKokLeong <ang.kok.leong.development@gmail.com>
2024-01-19 08:43:15 +0000

    modified the method to copy the file at later stage using the ONBUILD keyword

--- apache_web_server_docker_setup/copy_sshd_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if test ! -d /etc/ssh/sshd_config.d; then 
-        mkdir /etc/ssh/sshd_config.d 
-        cat sshd_config > /etc/ssh/sshd_config.d 
-    else 
-        cat sshd_config > /etc/ssh/sshd_config.d 
-fi
